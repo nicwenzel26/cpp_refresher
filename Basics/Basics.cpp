a66ecfd258a01234f9f6689f8833e01ac65a37ed
nicwenzel26 <nwenzel@mymail.mines.edu>
2026-09-24 16:43:08 -0600

    Added practice while loop and switch statement

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -4,14 +4,23 @@ int main()
 {
     int x = 0;
 
-    // Loop to print values of x from 0 to 9
-    while (x < 10)
+    // Practice while loop
+    while (x < 11)
     {
-        std::cout << "x = " << x << std::endl;
-        x++;
+      std::cout << "x = " << x << std::endl;
+      x += 1;
+
+      // Practice switch statement
+      switch (x)
+      {
+        case (5): // If x is 5 print this
+          std::cout << "Halfway!" << std::endl;
+          break;
+
+        default: // If x is not 5 do nothing 
+          break;
+      }
     }
-
-
 
     return 0;
 }
