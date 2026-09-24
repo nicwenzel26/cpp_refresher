40755537b605a294adc1fbbe21c372b7efd8e8c6
nicwenzel26 <nwenzel@mymail.mines.edu>
2026-09-24 16:24:55 -0600

    Created a Basics file with practice while loop

---
+++ Basics/Basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int x = 0;
+
+    // Loop to print values of x from 0 to 9
+    while (x < 10)
+    {
+        std::cout << "x = " << x << std::endl;
+        x++;
+    }
+
+
+
+    return 0;
+}
